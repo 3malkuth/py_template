e88e79c709b774cb707c55ad94043ba896d2ba96
3malkuth <107264938+3malkuth@users.noreply.github.com>
2024-04-08 14:01:57 +0000

    updated tests

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,4 +1,7 @@
-from module1.module1 import add
+from module1.module1 import add, subtract
 
 def test_add() -> None:
     assert add(1, 2) == 3
+
+def test_subtract() -> None:
+    assert subtract(2, 1) == 1
